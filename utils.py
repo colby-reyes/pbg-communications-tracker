c2e29f19986d59949984faa1dd056671d92dd813
colby-reyes <colby.n.reyes@gmail.com>
2023-11-03 19:15:40 +0000

    re-enable cache; add except

--- utils.py
+++ utils.py
@@ -16,32 +16,49 @@ class VersionInfo:
     contact = "colbyr@hs.uci.edu"
 
 
-#@st.cache_data(show_spinner=False)
+@st.cache_data(show_spinner=False)
 def GetSharepointSpread(
     sheetname: str,
     url: str = st.secrets["URL"],
     username: str = st.secrets["USERNAME"],
     password: str = st.secrets["PWD"],
 ):
+    try:
+        ctx_auth = AuthenticationContext(url)
+        if ctx_auth.acquire_token_for_user(username, password):
+            ctx = ClientContext(url, ctx_auth)
+            # web = ctx.web
+            # ctx.load(web)
+            ctx.execute_query()
+            print("Authentication successful")
 
-    ctx_auth = AuthenticationContext(url)
-    if ctx_auth.acquire_token_for_user(username, password):
-        ctx = ClientContext(url, ctx_auth)
-        # web = ctx.web
-        # ctx.load(web)
-        ctx.execute_query()
-        print("Authentication successful")
+        response = File.open_binary(ctx, url)
 
-    response = File.open_binary(ctx, url)
+        # %% save data to BytesIO stream
+        bytes_file_obj = io.BytesIO()
+        bytes_file_obj.write(response.content)
+        bytes_file_obj.seek(0)  # set file object to start
 
-    # %% save data to BytesIO stream
-    bytes_file_obj = io.BytesIO()
-    bytes_file_obj.write(response.content)
-    bytes_file_obj.seek(0)  # set file object to start
+        # %% read excel file and each sheet into pandas dataframe
+        df = pd.read_excel(bytes_file_obj, sheet_name=sheetname)
+        return df
+    except: 
+        ctx_auth = AuthenticationContext(url)
+        if ctx_auth.acquire_token_for_user(username, password):
+            ctx = ClientContext(url, ctx_auth)
+            # web = ctx.web
+            # ctx.load(web)
+            ctx.execute_query()
+            print("Authentication successful")
 
-    # %% read excel file and each sheet into pandas dataframe
-    
-    df = pd.read_excel(bytes_file_obj, sheet_name=sheetname)
-    return df
+        response = File.open_binary(ctx, url)
 
+        # %% save data to BytesIO stream
+        bytes_file_obj = io.BytesIO()
+        bytes_file_obj.write(response.content)
+        bytes_file_obj.seek(0)  # set file object to start
+
+        # %% read excel file and each sheet into pandas dataframe
+        df = pd.read_excel(bytes_file_obj, sheet_name=sheetname)
+        return df
 
